be24c5caecbd2e198a8abd85023dc9d06a837582
Frost Ming <mianghong@gmail.com>
2024-01-30 14:44:42 +0800

    Update service.py to use lazy import

--- service.py
+++ service.py
@@ -1,14 +1,8 @@
 import bentoml
 
-from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams
 from typing import Optional, AsyncGenerator, List
 
 MAX_TOKENS = 1024
-SAMPLING_PARAM = SamplingParams(max_tokens=MAX_TOKENS)
-ENGINE_ARGS = AsyncEngineArgs(
-    model='meta-llama/Llama-2-7b-chat-hf',
-    max_model_len=MAX_TOKENS
-)
 PROMPT_TEMPLATE = """<s>[INST] <<SYS>>
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
@@ -28,11 +22,21 @@ If a question does not make any sense, or is not factually coherent, explain why
 )
 class VLLMService:
     def __init__(self) -> None:
+        from vllm import AsyncEngineArgs, AsyncLLMEngine
+
+        ENGINE_ARGS = AsyncEngineArgs(
+            model='meta-llama/Llama-2-7b-chat-hf',
+            max_model_len=MAX_TOKENS
+        )
+        
         self.engine = AsyncLLMEngine.from_engine_args(ENGINE_ARGS)
         self.request_id = 0
 
     @bentoml.api
     async def generate(self, prompt: str = "Explain superconductors like I'm five years old", tokens: Optional[List[int]] = None) -> AsyncGenerator[str, None]:
+        from vllm import SamplingParams
+
+        SAMPLING_PARAM = SamplingParams(max_tokens=MAX_TOKENS)
         prompt = PROMPT_TEMPLATE.format(user_prompt=prompt)
         stream = await self.engine.add_request(self.request_id, prompt, SAMPLING_PARAM, prompt_token_ids=tokens)
         self.request_id += 1
