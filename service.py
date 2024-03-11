6eef44e0673fffe4cf8ec2444015d967758c5cea
Zhao Shenyang <dev@zsy.im>
2024-03-12 04:35:20 +0800

    improv: add openai chat template feature (#13)

--- service.py
+++ service.py
@@ -9,11 +9,10 @@ from bentovllm_openai.utils import openai_endpoints
 
 
 MAX_TOKENS = 1024
-PROMPT_TEMPLATE = """<s>[INST] <<SYS>>
+PROMPT_TEMPLATE = """<s>[INST]
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-<</SYS>>
 
 {user_prompt} [/INST] """
 
