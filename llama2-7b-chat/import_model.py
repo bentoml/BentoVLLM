6925e5f64d526169ab24fc8e4720a67ae48daefd
Zhao Shenyang <dev@zsy.im>
2024-03-19 05:20:05 +0800

    add llama-7b-chat model

---
+++ llama2-7b-chat/import_model.py
@@ -0,0 +1,23 @@
+import bentoml
+
+from service import MODEL_ID, BENTO_MODEL_TAG
+
+def import_model(model_id, bento_model_tag):
+
+    import torch
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_ID,
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+    )
+
+    with bentoml.models.create(bento_model_tag) as bento_model_ref:
+        tokenizer.save_pretrained(bento_model_ref.path)
+        model.save_pretrained(bento_model_ref.path)
+
+
+if __name__ == "__main__":
+    import_model(MODEL_ID, BENTO_MODEL_TAG)    
