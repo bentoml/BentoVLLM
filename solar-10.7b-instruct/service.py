1e7f7943738247939f1757ae0ed388d728076229
Alex Liu <83865371+lycheel1@users.noreply.github.com>
2024-03-19 20:03:37 +0000

    added solar instruct model for vllm (#19)
    * added solar instruct model for vllm
    
    * minor fix for naming

---
+++ solar-10.7b-instruct/service.py
@@ -0,0 +1,60 @@
+import uuid
+from typing import AsyncGenerator
+
+import bentoml
+from annotated_types import Ge, Le
+from typing_extensions import Annotated
+
+from bentovllm_openai.utils import openai_endpoints
+
+
+MAX_TOKENS = 1024
+PROMPT_TEMPLATE = """### User:
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+
+{user_prompt}
+### Assistant: """
+
+MODEL_ID = "upstage/SOLAR-10.7B-Instruct-v1.0"
+
+@openai_endpoints(served_model=MODEL_ID)
+@bentoml.service(
+    name="bentovllm-solar-instruct-service",
+    traffic={
+        "timeout": 300,
+    },
+    resources={
+        "gpu": 1,
+        "gpu_type": "nvidia-l4",
+    },
+)
+class VLLM:
+    def __init__(self) -> None:
+        from vllm import AsyncEngineArgs, AsyncLLMEngine
+        ENGINE_ARGS = AsyncEngineArgs(
+            model=MODEL_ID,
+            max_model_len=MAX_TOKENS,
+            gpu_memory_utilization=0.95
+        )
+        
+        self.engine = AsyncLLMEngine.from_engine_args(ENGINE_ARGS)
+
+    @bentoml.api
+    async def generate(
+        self,
+        prompt: str = "Explain superconductors like I'm five years old",
+        max_tokens: Annotated[int, Ge(128), Le(MAX_TOKENS)] = MAX_TOKENS,
+    ) -> AsyncGenerator[str, None]:
+        from vllm import SamplingParams
+
+        SAMPLING_PARAM = SamplingParams(max_tokens=max_tokens)
+        prompt = PROMPT_TEMPLATE.format(user_prompt=prompt)
+        stream = await self.engine.add_request(uuid.uuid4().hex, prompt, SAMPLING_PARAM)
+
+        cursor = 0
+        async for request_output in stream:
+            text = request_output.outputs[0].text
+            yield text[cursor:]
+            cursor = len(text)
